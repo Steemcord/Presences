ddd96f54ad02edf64ca88ac9910af95ff74a324d
Snazzah <suggesttosnazzy@gmail.com>
2020-11-04 01:30:33 -0600

    Make CMM insert script and update urls

--- util/combineMetadata.ts
+++ util/combineMetadata.ts
@@ -1,16 +1,25 @@
 import * as fs from 'fs';
 import { green, blue } from 'chalk';
 import { sync as glob } from 'glob';
+import { posix } from 'path';
 
 const metaFiles = glob('./presences/*/*/metadata.json', {
   ignore: ['**/node_modules/**', '**/@types/**'],
   absolute: true
 });
 
-const loadMetadata = (path: string): unknown =>
+const loadMetadata = (path: string): { script_url?: string; update_url?: string; } =>
     JSON.parse(fs.readFileSync(path, 'utf-8'));
 
 console.log(blue(`Combining metadata... (${metaFiles.length})`));
-const multiMetadata = metaFiles.map(mf => loadMetadata(mf));
+const multiMetadata = metaFiles.map(mf => {
+  const metadata = loadMetadata(mf);
+  const relativeDir = posix.relative(posix.join(__dirname, '..'), posix.join(mf, '..'));
+  return {
+    ...metadata,
+    script_url: `https://raw.githack.com/Steemcord/Presences/master/${relativeDir}/index.ts`,
+    update_url: `https://raw.githack.com/Steemcord/Presences/master/${relativeDir}/metadata.json`
+  };
+});
 fs.writeFileSync('./multi_metadata_result.json', JSON.stringify(multiMetadata));
 console.log(green(`✔ Wrote ${metaFiles.length} files into multi_metadata_result.json`));
