5204f10d268be60a04514ff701e2ca198cfef8d8
Snazzah <suggesttosnazzy@gmail.com>
2020-11-09 20:31:03 -0600

    Move update metadata to a different folder

--- util/combineMetadata.ts
+++ util/combineMetadata.ts
@@ -8,6 +8,13 @@ const metaFiles = glob('./presences/*/*/metadata.json', {
   absolute: true
 });
 
+// Create folder if not found
+try {
+  fs.accessSync('./_metadata', fs.constants.F_OK);
+} catch (e) {
+  fs.mkdirSync('./_metadata');
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const loadMetadata = (path: string): any =>
     JSON.parse(fs.readFileSync(path, 'utf-8'));
@@ -17,11 +24,15 @@ const multiMetadata = metaFiles.map(mf => {
   const metadata = loadMetadata(mf);
   const relativeDir = posix.relative(posix.join(__dirname, '..'), posix.join(mf, '..'));
   if(metadata.$schema) delete metadata.$schema;
-  return {
+  const parsedMetadata = {
     ...metadata,
     script_url: encodeURI(`https://raw.githack.com/Steemcord/Presences/master/${relativeDir}/index.ts`),
-    update_url: encodeURI(`https://raw.githack.com/Steemcord/Presences/master/${relativeDir}/metadata.json`)
+    update_url: encodeURI(`https://raw.githack.com/Steemcord/Presences/master/_metadata/${metadata.app_id}.json`)
   };
+  // Write update file
+  fs.writeFileSync(`./_metadata/${metadata.app_id}.json`, JSON.stringify(parsedMetadata));
+  console.log(green(`✔ Wrote _metadata/${metadata.app_id}.json (${metadata.name})`));
+  return parsedMetadata;
 });
 fs.writeFileSync('./multi_metadata.json', JSON.stringify(multiMetadata));
-console.log(green(`✔ Wrote ${metaFiles.length} files into multi_metadata.json`));
+console.log(green(`\n\n✔ Wrote ${metaFiles.length} files into multi_metadata.json`));
