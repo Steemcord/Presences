06b7da631d4e2c57fc00bf5c52734a513eec5462
Snazzah <suggesttosnazzy@gmail.com>
2021-04-18 09:39:43 -0500

    Update CS:GO presence

--- presences/C/Counter-Strike Global Offensive/index.ts
+++ presences/C/Counter-Strike Global Offensive/index.ts
@@ -3,7 +3,15 @@ const presence = new Presence({
   module
 });
 
-// let lastTimestamp = 0;
+let lastTimestamp = 0,
+  lastState: string = null;
+
+function checkState(state: string) {
+  if (lastState !== state) {
+    lastState = state;
+    lastTimestamp = Date.now();
+  }
+}
 
 const Maps: { [key: string]: string } = {\
   ar_dizzy: 'Dizzy',
@@ -18,12 +26,14 @@ const Maps: { [key: string]: string } = {\
   cs_office: 'Office',
   de_anubis: 'Anubis',
   de_austria: 'Austria',
+  de_ancient: 'Ancient',
   de_bank: 'Bank',
   de_cache: 'Cache',
   de_canals: 'Canals',
   de_cbble: 'Cobblestone',
   de_chlorine: 'Chlorine',
   de_dust2: 'Dust II',
+  de_engage: 'Engage',
   de_inferno: 'Inferno',
   de_lake: 'Lake',
   de_mirage: 'Mirage',
@@ -40,6 +50,7 @@ const Maps: { [key: string]: string } = {\
   de_train: 'Train',
   de_vertigo: 'Vertigo',
   dz_blacksite: 'Blacksite',
+  dz_frostbite: 'Frostbite',
   dz_junglety: 'Jungle',
   dz_sirocco: 'Sirocco',
   gd_rialto: 'Rialto',
@@ -52,25 +63,30 @@ const GameModeNames: { [key: string]: string } = {
   deathmatch: 'Deathmatch',
   skirmish: 'War Games',
   survival: 'Danger Zone',
+  coopmission: 'Co-op Mission',
+  cooperative: 'Co-op',
 }
 
 const SkirmishNames: { [key: string]: string } = {
   mg_skirmish_flyingscoutsman: 'Flying Scoutsman',
   mg_skirmish_armsrace: 'Arms Race',
   mg_skirmish_demolition: 'Demolition',
+  mg_skirmish_retakes: 'Retakes',
 }
 
 presence.on('richPresenceUpdate', async data => {
   if (!data.presenceString) presence.clearActivity();
 
   // Main Menu
-  if (data.presence.steam_display === '#display_Menu')
+  if (data.presence.steam_display === '#display_Menu') {
+    checkState('menu');
     presence.setActivity({
       largeImageKey: 'csgo',
       state: 'Main Menu',
     });
   // Lobby
-  else if (data.presence.steam_display.startsWith('#display_Lobby')) {
+  } else if (data.presence.steam_display.startsWith('#display_Lobby')) {
+    checkState('lobby');
     const mode = GameModeNames[data.presence['game:mode']] || data.presence['game:mode'];
     let groupName = 'map';
     let groupCount = data.presence['game:mapgroupname'].split(',').length;
@@ -87,6 +103,7 @@ presence.on('richPresenceUpdate', async data => {
     presence.setActivity(presenceData);
   // Spectating
   } else if (data.presence.steam_display.startsWith('#display_Watch')) {
+    checkState('watch');
     const map = Maps[data.presence['game:map']];
     const mode = GameModeNames[data.presence['game:mode']] || data.presence['game:mode'];
     const skirmishMode = (data.presence['game:mapgroupname'] && data.presence['game:mode'] === 'skirmish' && SkirmishNames[data.presence['game:mapgroupname']])
@@ -101,6 +118,7 @@ presence.on('richPresenceUpdate', async data => {
     presence.setActivity(presenceData);
   // Game: Known Map
   } else if (data.presence.steam_display.startsWith('#display_GameKnownMap') || data.presence.steam_display.startsWith('#display_GameWkshpMap')) {
+    checkState('game');
     const map = Maps[data.presence['game:map']];
     const mode = GameModeNames[data.presence['game:mode']] || data.presence['game:mode'];
     const isWorkshop = data.presence.steam_display.includes('Wkshp');
@@ -111,6 +129,7 @@ presence.on('richPresenceUpdate', async data => {
       largeImageText: map || data.presence['game:map'].split('/').reverse()[0],
       state: `Playing ${mode}`,
       details: map,
+      startTimestamp: lastTimestamp
     };
     if (data.presence.steam_display.endsWith('Score'))
       presenceData.details = `${isWorkshop ? 'Workshop Map' : (map || '')} ${data.presence['game:score']}`;
